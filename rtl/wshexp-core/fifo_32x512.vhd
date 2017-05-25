2e0965420c2d066e2af3340e7dae99198481f7d7
cretingame <arnaud.sautaux@gmail.com>
2017-05-24 17:37:23 -0700

    Added files

---
+++ rtl/wshexp-core/fifo_32x512.vhd
@@ -0,0 +1,91 @@
+--------------------------------------------------------------------------------
+--     (c) Copyright 1995 - 2010 Xilinx, Inc. All rights reserved.            --
+--                                                                            --
+--     This file contains confidential and proprietary information            --
+--     of Xilinx, Inc. and is protected under U.S. and                        --
+--     international copyright and other intellectual property                --
+--     laws.                                                                  --
+--                                                                            --
+--     DISCLAIMER                                                             --
+--     This disclaimer is not a license and does not grant any                --
+--     rights to the materials distributed herewith. Except as                --
+--     otherwise provided in a valid license issued to you by                 --
+--     Xilinx, and to the maximum extent permitted by applicable              --
+--     law: (1) THESE MATERIALS ARE MADE AVAILABLE "AS IS" AND                --
+--     WITH ALL FAULTS, AND XILINX HEREBY DISCLAIMS ALL WARRANTIES            --
+--     AND CONDITIONS, EXPRESS, IMPLIED, OR STATUTORY, INCLUDING              --
+--     BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, NON-                 --
+--     INFRINGEMENT, OR FITNESS FOR ANY PARTICULAR PURPOSE; and               --
+--     (2) Xilinx shall not be liable (whether in contract or tort,           --
+--     including negligence, or under any other theory of                     --
+--     liability) for any loss or damage of any kind or nature                --
+--     related to, arising under or in connection with these                  --
+--     materials, including for any direct, or any indirect,                  --
+--     special, incidental, or consequential loss or damage                   --
+--     (including loss of data, profits, goodwill, or any type of             --
+--     loss or damage suffered as a result of any action brought              --
+--     by a third party) even if such damage or loss was                      --
+--     reasonably foreseeable or Xilinx had been advised of the               --
+--     possibility of the same.                                               --
+--                                                                            --
+--     CRITICAL APPLICATIONS                                                  --
+--     Xilinx products are not designed or intended to be fail-               --
+--     safe, or for use in any application requiring fail-safe                --
+--     performance, such as life-support or safety devices or                 --
+--     systems, Class III medical devices, nuclear facilities,                --
+--     applications related to the deployment of airbags, or any              --
+--     other applications that could lead to death, personal                  --
+--     injury, or severe property or environmental damage                     --
+--     (individually and collectively, "Critical                              --
+--     Applications"). Customer assumes the sole risk and                     --
+--     liability of any use of Xilinx products in Critical                    --
+--     Applications, subject only to applicable laws and                      --
+--     regulations governing limitations on product liability.                --
+--                                                                            --
+--     THIS COPYRIGHT NOTICE AND DISCLAIMER MUST BE RETAINED AS               --
+--     PART OF THIS FILE AT ALL TIMES.                                        --
+--------------------------------------------------------------------------------
+
+--  Generated from component ID: xilinx.com:ip:fifo_generator:6.2
+
+
+-- You must compile the wrapper file fifo_32x512.vhd when simulating
+-- the core, fifo_32x512. When compiling the wrapper file, be sure to
+-- reference the XilinxCoreLib VHDL simulation library. For detailed
+-- instructions, please refer to the "CORE Generator Help".
+
+-- The synthesis directives "translate_off/translate_on" specified
+-- below are supported by Xilinx, Mentor Graphics and Synplicity
+-- synthesis tools. Ensure they are correct for your synthesis tool(s).
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+-- synthesis translate_off
+Library XilinxCoreLib;
+-- synthesis translate_on
+ENTITY fifo_32x512 IS
+	port (
+	rst: in std_logic;
+	wr_clk: in std_logic;
+	rd_clk: in std_logic;
+	din: in std_logic_vector(31 downto 0);
+	wr_en: in std_logic;
+	rd_en: in std_logic;
+	prog_full_thresh_assert: in std_logic_vector(9 downto 0);
+	prog_full_thresh_negate: in std_logic_vector(9 downto 0);
+	dout: out std_logic_vector(31 downto 0);
+	full: out std_logic;
+	empty: out std_logic;
+	valid: out std_logic;
+	prog_full: out std_logic);
+END fifo_32x512;
+
+ARCHITECTURE fifo_32x512_a OF fifo_32x512 IS
+
+
+
+BEGIN
+
+
+END fifo_32x512_a;
+
