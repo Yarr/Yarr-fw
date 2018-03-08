c832dfa491842e18dc1a53b1733290caf3d89882
Yasunori Sawada <yasunori.sawada@cern.ch>
2018-03-08 16:58:02 +0900

    Modified : sed command

--- syn/kc705/convert_xpressk7toKC705.sh
+++ syn/kc705/convert_xpressk7toKC705.sh
@@ -9,13 +9,13 @@ cp ../xpressk7/ddr3_yarr.vhd $OUTPUT_DDR3
 XPRESSK7="rst_s <= not rst_n_i;"
 KC705="rst_s <= rst_n_i;"
 
-sed -n -e "/$XPRESSK7/$KC705/p" $OUTPUT_BRAM
-sed -n -e "/$XPRESSK7/$KC705/p" $OUTPUT_DDR3
+sed -i -e "s/$XPRESSK7/$KC705/g" $OUTPUT_BRAM
+sed -i -e "s/$XPRESSK7/$KC705/g" $OUTPUT_DDR3
 
 XPRESSK7="arstn_s <= sys_rst_n_i or rst_n_i;"
 KC705="arstn_s <= sys_rst_n_i or not rst_n_i;"
 
-sed -n -e "/$XPRESSK7/$KC705/p" $OUTPUT_BRAM
-sed -n -e "/$XPRESSK7/$KC705/p" $OUTPUT_DDR3
+sed -i -e "s/$XPRESSK7/$KC705/g" $OUTPUT_BRAM
+sed -i -e "s/$XPRESSK7/$KC705/g" $OUTPUT_DDR3
 
 echo "Done!"
