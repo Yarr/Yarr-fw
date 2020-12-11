ddc3807ea9d509fc10a452cb38ca05705961dddc
Lucas Cendes <lucascendes@gmail.com>
2020-12-10 17:56:52 -0800

    Updated Manisfest.py file in tx-core folder

--- rtl/tx-core/Manifest.py
+++ rtl/tx-core/Manifest.py
@@ -1,8 +1,7 @@
 files = ["serial_port.vhd",
          "trigger_unit.vhd",
          "tx_channel.vhd",
-         "encoder.vhd",
-         "trig_shift_reg.vhd",
+         "trigger_encoder.vhd",
          "trig_code_gen.vhd",
          "wb_tx_core.vhd"
          ]
