bb5f3bd67e59cd5aea7207ad6dae9c61db257944
Timon Heim <timon.heim@cern.ch>
2021-08-04 14:36:28 -0700

    Update

--- syn/kintex7/version.vhd
+++ syn/kintex7/version.vhd
@@ -3,5 +3,5 @@ use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
 
 package version_pkg is
-    constant c_FW_VERSION : std_logic_vector(31 downto 0) := x"0635aa62";
+    constant c_FW_VERSION : std_logic_vector(31 downto 0) := x"08a9cdd6";
 end version_pkg;
